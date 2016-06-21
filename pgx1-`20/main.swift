dc0ca585aeacc00b114f2fb17f845225163a8a19
zrlyx <20141105042hyb@admindeiMac-007.local>
2016-06-21 10:43:13 +0800

    20个数从小到大排列
    Swift实现快速排序的代码非常的简洁,只需要几行代码就能实现原本冒泡排序解决的问题。而且方法多种多样。闭包的的写法也很多。
    mages = images.sorted({ (image1: imageFile, image2: imageFile) -> Bool
    in return image1.fileID > image2.fileID })
    images = images.sorted({ image1, image2 in return image1.fileID >
    image2.fileID })
    images = images.sorted({ image1, image2 in image1.fileID >
    image2.fileID })
    images = images.sorted({ $0.fileID > $1.fileID })

--- pgx1-`20/main.swift
+++ pgx1-`20/main.swift
@@ -8,5 +8,9 @@
 
 import Foundation
 
-print("Hello, World!")
 
+let numbers = [1,2,44,4,5,6,77,8,9,19,63,12,13,14,15,86,17,28,19,20]
+
+let numbersSorted = numbers.sort({(n1 : Int,n2 : Int) -> Bool in return n2 > n1})
+
+print(numbersSorted)
